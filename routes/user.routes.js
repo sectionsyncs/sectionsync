62dce03093453a21cf13c38c7baf7efcc8b19c51
AnilKJangir <107305368+AnilKJangir@users.noreply.github.com>
2025-05-15 14:00:02 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -381,15 +381,15 @@ router.get('/verify-email', async (req, res) => {
       process.env.JWT_SECRET
     );
 
-        // Send new cookie
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        });  
+    // Send new cookie
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });  
   
-      res.redirect('/user/account');
+        res.redirect('/');
     } catch (error) {
       res.status(500).send('Verification failed', error);
     }
